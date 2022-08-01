747b5dfaf821a9730b7ffd4e2160c619af6cd95a
Poatri-US <83054753+Poatri-US@users.noreply.github.com>
2022-08-01 21:52:28 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+#SmartWorkByPo3
+#Smart Work Not Hard Work
+calculation = input("Type calculation:\n")
+print("Answer = " + str(eval(calculation)))
+
+
+
+#When i divide 0 by 0 , i get this error , need to specify with elif calculation = 0/0 then print print("Answer = Undetermined")
+#line 4, in <module>
+#    print("Answer = " + str(eval(calculation)))
+#  File "<string>", line 1, in <module>
+#ZeroDivisionError: division by zero
